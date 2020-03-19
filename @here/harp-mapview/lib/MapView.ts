35bac750e00643feb84518b05962376f21645deb
Krystian Kostecki <ext-krystian.kostecki@here.com>
2020-03-19 10:39:32 +0100

    HARP-5925: Provide world anchors support in MapView api.
    Extend MapView interface to provide root node for world positioned objects
    (anchors). The position of those objects is updated according to MapView
    camera setup, but it is stable across updates, preserving camera relative
    position.
    
    Signed-off-by: Krystian Kostecki <ext-krystian.kostecki@here.com>

--- @here/harp-mapview/lib/MapView.ts
+++ @here/harp-mapview/lib/MapView.ts
@@ -92,7 +92,7 @@ if (isProduction) {
  *
  * Example:
  * ```typescript
- * const mesh: MapObject<THREE.Mesh> = new THREE.Mesh(geometry, material);
+ * const mesh: MapAnchor<THREE.Mesh> = new THREE.Mesh(geometry, material);
  * mesh.geoPosition = new GeoCoordinates(latitude, longitude, altitude);
  * mapView.mapAnchors.add(mesh);
  * ```
@@ -100,21 +100,43 @@ if (isProduction) {
  */
 export type MapAnchor<T extends THREE.Object3D = THREE.Object3D> = T & {
     /**
-     * The position of this [[MapObject]] in [[GeoCoordinates]].
+     * The position of this [[MapAnchor]] in [[GeoCoordinates]].
      */
     geoPosition?: GeoCoordinates;
     /**
-     * The position of this [[MapObject]] in world coordinates (and units).
+     * Flag defining is the object may be picked.
+     *
+     * @note By default all objects are pickable even if this flag is undefined.
+     */
+    pickable?: boolean;
+};
+
+/**
+ * An interface describing [[THREE.Object3D]]s anchored on given world coordinates.
+ *
+ * Example:
+ * ```typescript
+ * const mesh: WorldAnchor<THREE.Mesh> = new THREE.Mesh(geometry, material);
+ * mesh.worldPosition = new Vector3(x, y, z);
+ * mesh.pickable = false;
+ * mapView.worldAnchors.add(mesh);
+ * ```
+ *
+ */
+export type WorldAnchor<T extends THREE.Object3D = THREE.Object3D> = T & {
+    /**
+     * The position of this [[WorldAnchor]] in world coordinates (and units).
      *
      * Word coordinates anchors may be used for objects that has not exact relation to the
      * place on the Earth globe or map. This may include light sources, special cameras, effects.
-     *
-     * @note Geo-coordinates anchoring has higher priority and should be used whenever
-     * object has position related to the globe, then world coords may be left undefined.
-     * In other cases (object position not related to globe) leave geo-coords undefined and
-     * provide the world coordinates as the base for object positioning.
      */
     worldPosition?: THREE.Vector3;
+    /**
+     * Flag defining is the object may be picked.
+     *
+     * @note By default all objects are pickable even if this flag is undefined.
+     */
+    pickable?: boolean;
 };
 
 export enum MapViewEventNames {
@@ -736,6 +758,7 @@ export class MapView extends THREE.EventDispatcher {
     private readonly m_fog: MapViewFog = new MapViewFog(this.m_scene);
     private readonly m_mapTilesRoot = new THREE.Object3D();
     private readonly m_mapAnchors = new THREE.Object3D();
+    private readonly m_worldAnchors = new THREE.Object3D();
 
     private m_animationCount: number = 0;
     private m_animationFrameHandle: number | undefined;
@@ -1634,9 +1657,24 @@ export class MapView extends THREE.EventDispatcher {
      * All (first level) children of this node will be positioned in world space according to the
      * [[MapAnchor.geoPosition]].
      * Deeper level children can be used to position custom objects relative to the anchor node.
+     *
+     * @see worldAnchors
      */
     get mapAnchors(): THREE.Object3D {
         return this.m_mapAnchors;
+    }
+
+    /**
+     * The root node for user's defined objects that will be positioned by world coordinates.
+     *
+     * This objects are transformed according to camera setup, but are not _attached_ to map
+     * geo position. Such anchors may be used to add custom rendering geometry of effects that are
+     * moving or just positioned in world space, i.e. light source, comet, spaceship, etc.
+     *
+     * @see mapAnchors.
+     */
+    get worldAnchors(): THREE.Object3D {
+        return this.m_worldAnchors;
     }
 
     /**
@@ -2873,12 +2911,13 @@ export class MapView extends THREE.EventDispatcher {
             if (childObject.geoPosition !== undefined) {
                 this.projection.projectPoint(childObject.geoPosition, childObject.position);
                 childObject.position.sub(this.camera.position);
-            } else if (childObject.worldPosition !== undefined) {
+            }
+        });
+        this.m_worldAnchors.children.forEach((childObject: WorldAnchor) => {
+            if (childObject.worldPosition !== undefined) {
                 const wp = childObject.worldPosition;
                 childObject.position.set(wp.x, wp.y, wp.z);
                 childObject.position.sub(this.camera.position);
-            } else {
-                return;
             }
         });
 
@@ -3489,6 +3528,7 @@ export class MapView extends THREE.EventDispatcher {
 
         this.m_scene.add(this.m_mapTilesRoot);
         this.m_scene.add(this.m_mapAnchors);
+        this.m_scene.add(this.m_worldAnchors);
     }
 
     private createTextRenderer(): TextElementsRenderer {
